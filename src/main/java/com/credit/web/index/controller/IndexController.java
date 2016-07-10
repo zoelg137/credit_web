2c31c71148484e33d01a5a689aa6fd9edaa02a07
apollo2099 <liaoxiongjian.2008@163.com>
2016-07-10 18:27:26 +0800

    4：调整spring jsp页面映射关系。

---
+++ src/main/java/com/credit/web/index/controller/IndexController.java
@@ -0,0 +1,18 @@
+package com.credit.web.index.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.gvtv.manage.base.controller.BaseController;
+
+@Controller
+
+public class IndexController extends BaseController{
+	@RequestMapping(value="/index")
+	public ModelAndView index(){
+		ModelAndView mv = super.getModelAndView();
+		mv.setViewName("index_main");
+		return mv;
+	}
+}
